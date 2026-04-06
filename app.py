399feb862cbdecc93523e607b8b05708619ea171
Ashborne-lab <138369125+Ashborne-lab@users.noreply.github.com>
2026-04-06 13:30:01 +0530

    Upgrade UI: Add Tailwind CSS Dashboard

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>DevOps Pipeline Active</h1><p id='status'>Application is running successfully.</p>"
+    # This tells Flask to look in the 'templates' folder and serve the dashboard
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
